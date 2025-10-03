9935f1c58314239e75854859cfdac602b388ba0a
Tauheed Elahee <online-accounts@tauheed-elahee.ca>
2025-10-03 15:11:57 -0400

    Updated api-test.ts

--- src/pages/api/api-test.ts
+++ src/pages/api/api-test.ts
@@ -1,8 +1,13 @@
-export async function GET({params, request)) {
+import type { APIRoute } from 'astro';
+
+export const GET: APIRoute = async () => {
   return new Response(
     JSON.stringify({
       name: 'Astro',
       url: 'https://astro.build/'
-    })
-  )
-}})
+    }),
+    {
+      headers: { 'Content-Type': 'application/json' }
+    }
+  );
+};
