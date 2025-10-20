e6e4258b47b10950e8bc2c704c1eb9489bc811c6
Tauheed Elahee <online-accounts@tauheed-elahee.com>
2025-10-20 14:02:19 -0400

    Updated astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,11 @@
 import { defineConfig } from 'astro/config';
-import cloudflare from '@astrojs/cloudflare';
+import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: cloudflare({
-      platformProxy: {
-        enabled: true
-      }
+  adapter: node({
+    mode: 'standalone'
   }),
   server: {
     host: true,
