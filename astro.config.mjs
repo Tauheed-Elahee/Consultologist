acd609a199f31e8ec96055f956060c556a905f93
Tauheed Elahee <online-accounts@tauheed-elahee.com>
2025-10-31 00:21:47 -0400

    Change astro config from server to static

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,20 @@
-import { defineConfig } from 'astro/config';
-import node from '@astrojs/node';
+import { defineConfig } from "astro/config";
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  adapter: node({
-    mode: 'standalone'
-  }),
-  server: {
-    host: true,
-    port: 4321
-  },
-  vite: {
-    assetsInclude: ['**/*.liquid'],
-    json: {
-      stringify: false
-    }
-  }
+	output: "static",
+	adapter: node({
+		mode: "standalone",
+	}),
+	server: {
+		host: true,
+		port: 4321,
+	},
+	vite: {
+		assetsInclude: ["**/*.liquid"],
+		json: {
+			stringify: false,
+		},
+	},
 });
