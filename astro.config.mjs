85913044dcc8809e44a8e9ca4d64d5863e16b212
Tauheed Elahee <online-accounts@tauheed-elahee.com>
2025-10-31 11:58:08 -0400

    Remove import of astro node to force static build.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,8 @@
-import node from "@astrojs/node";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
 	output: "static",
-	adapter: node({
-		mode: "standalone",
-	}),
 	server: {
 		host: true,
 		port: 4321,
